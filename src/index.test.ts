4aeb89a1562870169a8f781744b720729b4af04c
Justin Chi <jchi@memsql.com>
2020-11-05 16:44:15 -0800

    moar simplifying

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,10 +4,10 @@ test("sanity", () => {
     expect(sum(1, 2)).toEqual(3);
 });
 
-test("studio config", () => {
+test("config", () => {
     expect(DEFAULT_CONFIG).toEqual(DEFAULT_CONFIG);
 });
 
-test("studio config wrapped in function", () => {
+test("config wrapped in function", () => {
     expect(getDefaultConfig(true)).toEqual(getDefaultConfig(true));
 });
